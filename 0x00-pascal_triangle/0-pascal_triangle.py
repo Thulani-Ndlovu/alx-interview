d41a02fb7db1a77f00027dd690a0cccb77e72265
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-04 01:38:04 +0200

    Task 0. Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,13 @@
+'''Pascal Triangle function'''
+
+
 def pascal_triangle(n):
     """Generates the nth row of Pascal's triangle."""
-
-    # empty list that stores the rows of the pascal's triangle
     pascalTriangle = []
 
-    # Return an empty list if n <= 0:
     if n <= 0:
         return pascalTriangle
 
-    # Loop to fill the rows of the triangle
     for i in range(n):
         pascalTriangle.append([])
         for j in range(i + 1):
