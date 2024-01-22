c7a80e9c3e914ca624ca78fe6962d80966f98483
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-22 20:37:27 +0200

    Task 0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Minimum number of operations'''
+
+
+def minOperations(n):
+    '''Returns number of operations'''
+    if n <= 0:
+        return 0
+
+    def dp(operation):
+        '''Depth search'''
+        for i in range(2, int(operation ** 0.5) + 1):
+            if operation % i == 0:
+                return i + dp(operation // i)
+        return operation
+
+    return dp(n)
